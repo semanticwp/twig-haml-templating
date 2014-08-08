a71a06d6c29e5bd1fbcd06147daa59d2a402b4f2
andrea rota <a@xelera.eu>
2014-08-08 14:12:33 +0100

    wrapped function in class

--- lib/SemanticWP/Templating/TwigHAMLEngine.php
+++ lib/SemanticWP/Templating/TwigHAMLEngine.php
@@ -64,16 +64,3 @@ class TwigHAMLEngine {
     return $this->twig->render($template_file, $data);
   }
 }
-
-/**
- * Thin wrapper for SemanticWP's Twig+MtHaml templating, meant to be
- * an almost-drop-in replacement for WordPress' get_template_part()
- *
- * TODO: look for templates up the starter theme chain (e.g. final
- * theme, foundoots, roots)
- */
-function get_template_part($template_file, $data) {
-  $engine = new TwigHAMLEngine();
-  echo $engine->render_template($template_file . '.haml', $data);
-}
-
